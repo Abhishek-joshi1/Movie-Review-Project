a8b9a814ba75d9399341531e1c891b4585c5f8a1
Abhishek-joshi1 <abhisbekjoshi@gmail.com>
2025-02-10 08:00:06 -0800

    Completed the Projected

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from "./components/Header/Header.jsx"
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import Header from './components/Header/Header.jsx';
+import MovieDetail from "./pages/MovieDetail.jsx"
+import MovieList from "./components/MovieList/MovieList.jsx"
 
 function App() {
   return (
-    <>
     <div className="App">
-    <Router>
-      <Header />
-      <Routes>
-        <Route index element={<h1>Hello World</h1>}></Route>
-        <Route path="movie/:id" element={<h1>Movie details Page</h1>}></Route>
-        <Route path="movies/:type" element={<h1>Movie List Page</h1>}></Route>
-        <Route path="/*" element={<h1>Error Page</h1>}></Route>
-      </Routes>
-    </Router>
+        <Router>
+          <Header />
+            <Routes>
+                <Route index element={<Home />}></Route>
+                <Route path="movie/:id" element={<MovieDetail />}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
